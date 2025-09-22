2e23a95ba153c41a4b6f75ff68b6efc98d14ba7e
dvir_levy <69078741+jarman155@users.noreply.github.com>
2025-09-22 13:13:37 +0300

    finished the first task

--- simpleRobotCode/src/main/java/frc/robot/RobotContainer.java
+++ simpleRobotCode/src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.chassis.commands.Drive;
 import frc.robot.chassis.commands.auto.FieldTarget;
 import frc.robot.chassis.commands.auto.FieldTarget.ELEMENT_POSITION;
 import frc.robot.chassis.commands.auto.FieldTarget.FEEDER_SIDE;
@@ -18,17 +19,14 @@ import frc.robot.leds.subsystems.LedManager;
 import frc.robot.robot1.arm.subsystems.Arm;
 import frc.robot.robot1.gripper.subsystems.Gripper;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.utils.CommandController;
+import frc.robot.utils.CommandController.ControllerType;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
+
 public class RobotContainer {
   public static FEEDER_SIDE currentFeederSide;
   public static Gripper gripper;
@@ -37,50 +35,39 @@ public class RobotContainer {
   public static LedManager ledManager;
   public static boolean isRed;
   public static Robot1Strip robot1Strip;
-    public static boolean isComp = DriverStation.isFMSAttached();
-  // The robot's subsystems and commands are defined here...
+  public static boolean isComp = DriverStation.isFMSAttached();
+  public static CommandController driverController;
+  public static CommandController operatorController;
+
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   public static FieldTarget scoringTarget = new FieldTarget(POSITION.A, ELEMENT_POSITION.CORAL_LEFT, LEVEL.L3);
-  // Replace with CommandPS4Controller or CommandJoystick if needed
+
  
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+
   public RobotContainer() {
-    // Configure the trigger bindings
+    driverController = new CommandController(OperatorConstants.DRIVER_CONTROLLER_PORT, ControllerType.kXbox);
+    operatorController = new CommandController(OperatorConstants.OPERATOR_CONTROLLER_PORT, ControllerType.kXbox);
+
     configureBindings();
   }
 
   public static boolean isRed() {
     return isRed;
   }
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
+  private void configureBindings() {
+
 
   }
   public static boolean isComp() {
     return isComp;
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
+    chassis.setDefaultCommand(new Drive(chassis, driverController));
+
     return Autos.exampleAuto(m_exampleSubsystem);
   }
+
 }
